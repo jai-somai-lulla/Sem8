0236c592fa54a24758f3d9c49194c67ebe8e91d1
jai-somai-lulla <jai-somai-lulla@github.com>
2019-02-27 15:23:55 +0530

    Cartesian Product + MaxProduct

--- SoftComputing/Fuzzy/fuzzyop.py
+++ SoftComputing/Fuzzy/fuzzyop.py
@@ -38,7 +38,7 @@ class FuzzySet:
 		print self.f_set			
 	
 	@staticmethod
-	def minmax(x,y):
+	def maxmin(x,y):
 		print 'Max-Min'
 		#z = []
 		z = [[0 for i in range(x.shape[0])] for i in range(y.shape[1])] 
@@ -53,17 +53,66 @@ class FuzzySet:
 				for i in range (x1.shape[0]):
 					#print "Min"+str(x1[i])+","+str(y1[i])
 					mix[i]=min(x1[i],y1[i])
-				print mix
-				print max(mix)
+				#print mix
+				#print max(mix)
 				
 				z[row][col]=max(mix)
-				print 'ZZ::'
-				print z
-				print '--ZZ--'
+				#print 'ZZ::'
+				#print z
+				#print '--ZZ--'
 				col+=1
 			row+=1
 		print z			
-			#print ''			
+		
+	@staticmethod
+	def maxproduct(x,y):
+		print 'Max-Min'
+		#z = []
+		z = [[0 for i in range(x.shape[0])] for i in range(y.shape[1])] 
+		row=0
+		for x1 in x:
+			#print 'X:'+str(x1)
+			#row
+			col=0
+			for y1 in y.T:
+				#print 'Y:'+str(y1)
+				mix=[0,0,0]
+				for i in range (x1.shape[0]):
+					#print "Min"+str(x1[i])+","+str(y1[i])
+					mix[i]=(x1[i]*y1[i])
+				#print mix
+				#print max(mix)
+				
+				z[row][col]=max(mix)
+				#print 'ZZ::'
+				#print z
+				#print '--ZZ--'
+				col+=1
+			row+=1
+		print z			
+			#print ''
+			
+	@staticmethod
+	def cartProduct(a,b):
+		print 'Cart Product'
+		a.show()
+		b.show()
+		
+		z = [[0 for i in range(len(a.f_set))] for i in range(len(b.f_set))] 
+		
+		row=0
+		for k in a.f_set:
+			#print a.f_set[k]
+			col=0
+			for j in b.f_set:
+				#print b.f_set[j]
+				z[row][col]=min(a.f_set[k],b.f_set[j])	
+				col+=1
+			row+=1
+		print z	
+		return z
+		
+						
 				
 
 def main():
@@ -72,10 +121,16 @@ def main():
 	
 	b = FuzzySet({'x1': 0.8, 'x2': 0.2, 'x3': 1.0})
 	
+	
 	r1 = np.array([[1, 0, 0.7], [0.3, 0.2, 0], [0, 0.5, 1]])
 	r2 = np.array([[0.6, 0.6, 0], [0, 0.6, 0.1], [0, 0.1, 0]])	
-	FuzzySet.minmax(r1,r2)
 	
+	
+	FuzzySet.cartProduct(a,b)
+	return
+	
+	FuzzySet.maxmin(r1,r2)
+	FuzzySet.maxproduct(r1,r2)
 	return
 	 
 	print "\t--A--"
