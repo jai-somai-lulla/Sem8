f08bd17a3c9aa1b2940978fd2a3d218e2f862291
jai-somai-lulla <jai-somai-lulla@github.com>
2019-02-27 14:39:01 +0530

    Max-Min

--- SoftComputing/Fuzzy/fuzzyop.py
+++ SoftComputing/Fuzzy/fuzzyop.py
@@ -34,15 +34,36 @@ class FuzzySet:
 		C = FuzzySet(c)	
 		return C
 		
-	@staticmethod
-	def max_min(a,b):
-		
-		for k1 in a.f_set:
-		
-					
-	
 	def show(self):
 		print self.f_set			
+	
+	@staticmethod
+	def minmax(x,y):
+		print 'Max-Min'
+		#z = []
+		z = [[0 for i in range(x.shape[0])] for i in range(y.shape[1])] 
+		row=0
+		for x1 in x:
+			#print 'X:'+str(x1)
+			#row
+			col=0
+			for y1 in y.T:
+				#print 'Y:'+str(y1)
+				mix=[0,0,0]
+				for i in range (x1.shape[0]):
+					#print "Min"+str(x1[i])+","+str(y1[i])
+					mix[i]=min(x1[i],y1[i])
+				print mix
+				print max(mix)
+				
+				z[row][col]=max(mix)
+				print 'ZZ::'
+				print z
+				print '--ZZ--'
+				col+=1
+			row+=1
+		print z			
+			#print ''			
 				
 
 def main():
@@ -50,6 +71,13 @@ def main():
 	a = FuzzySet({'x1': 0.5, 'x2': 0.7, 'x3': 0.0})
 	
 	b = FuzzySet({'x1': 0.8, 'x2': 0.2, 'x3': 1.0})
+	
+	r1 = np.array([[1, 0, 0.7], [0.3, 0.2, 0], [0, 0.5, 1]])
+	r2 = np.array([[0.6, 0.6, 0], [0, 0.6, 0.1], [0, 0.1, 0]])	
+	FuzzySet.minmax(r1,r2)
+	
+	return
+	 
 	print "\t--A--"
 	a.show()
 	print ''
@@ -75,6 +103,7 @@ def main():
 	print '-----A=>B------'
 	c = FuzzySet.__or__(FuzzySet.__not__(a),b)
 	c.show()	
+	
 	
 	
 if __name__ == "__main__":
